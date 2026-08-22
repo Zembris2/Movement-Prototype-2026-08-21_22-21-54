9e3cbd576662b16aa88fc1ba79692b4047ec5e6d
Zembris2 <pingxgodx@gmail.com>
2026-08-22 20:36:03 +0700

    Add Unity project scaffold and player movement scene

---
+++ Assets/movement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
